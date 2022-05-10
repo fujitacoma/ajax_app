04413ad0b1bbc89c035b26df4d62dba99dbb8644
fujitacoma <john.vsvk.0424@gmail.com>
2022-05-11 00:14:54 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,5 @@
 class PostsController < ApplicationController
-
   def index
-    @posts = Post.all
-  end
-
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
+    @posts = Post.order(id: "DESC")
   end
 end
