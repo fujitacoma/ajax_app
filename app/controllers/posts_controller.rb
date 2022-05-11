098c38dac9a252f5dfa5db2b6aac1896bf48e025
fujitacoma <john.vsvk.0424@gmail.com>
2022-05-11 12:36:16 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.order(id: "DESC")
   end
-end
+ 
+   # コメントアウトする
+   # def new
+   # end
+ 
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index  # 追記する
+  end
+ 
+ end
+ 
